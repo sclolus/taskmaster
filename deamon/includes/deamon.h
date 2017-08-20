9f8ed90a0fad9c8e3e4b87680550b305d59f76df
Sebastien CLOLUS <sclolus@e3r13p4.42.fr>
2017-08-20 04:40:47 +0200

    dump

--- deamon/includes/deamon.h
+++ deamon/includes/deamon.h
@@ -6,7 +6,7 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 02:15:26 by sclolus           #+#    #+#             */
-/*   Updated: 2017/08/20 03:58:22 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/08/20 04:27:10 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,17 @@
 # include "libft.h"
 # include <stdio.h>
 # include <netinet/in.h>
+# include <arpa/inet.h>
 
 # define DEAMON_NAME "deamon"
 # define MAX_PENDING_CONNECTION 128
 # define OFFSETOF(type, member) ((uint64_t)(&((type*)0)->member))
+
+/*
+** DEBUG
+*/ //
+
+# define CHECK(x) do {ft_putendl_fd("_____", 2);ft_putendl_fd(#x, 2);ft_putendl_fd("_____", 2);} while (0);
 
 /*
 ** Error Handling
--- deamon/srcs/main.c
+++ deamon/srcs/main.c
@@ -6,14 +6,15 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 02:11:40 by sclolus           #+#    #+#             */
-/*   Updated: 2017/08/20 04:01:16 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/08/20 04:27:45 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "deamon.h"
 
-# define AALVES_IP "10.13.13.51441"
+# define AALVES_IP "10.13.13.2"
 # include <stdio.h>
+
 int	main(void)
 {
 	int					socketfd;
@@ -21,9 +22,11 @@ int	main(void)
 
 	ft_bzero(&serv_sock, sizeof(struct sockaddr));
 	serv_sock.sin_family = PF_INET;
-	serv_sock.sin_port = 7777;
+	serv_sock.sin_port = htons(7777);
 	ft_strcpy((char*)&serv_sock.sin_addr.s_addr, AALVES_IP);
-	serv_sock.sin_len = /* OFFSETOF(struct sockaddr, sa_data) */sizeof(serv_sock.sin_family) + sizeof(AALVES_IP);
+//	serv_sock.sin_addr.s_addr = INADDR_ANY;
+	serv_sock.sin_addr.s_addr = inet_addr("AALVES_IP");
+	serv_sock.sin_len = sizeof(serv_sock.sin_family) + sizeof(AALVES_IP);
 	if (-1 == (socketfd = socket(PF_INET, SOCK_STREAM, 0)))
 		perror(DEAMON_NAME " socket()" );
 	if (-1 == (bind(socketfd, (const struct sockaddr*)&serv_sock
@@ -31,8 +34,9 @@ int	main(void)
 		perror(DEAMON_NAME " bind()" );
 	if (-1 == (listen(socketfd, MAX_PENDING_CONNECTION)))
 		perror(DEAMON_NAME " listen()" );
-	if (-1 == accept(socketfd, &serv_sock, &(socklen_t){OFFSETOF(struct sin_addr, sin_addr.s_addr) + sizeof(AALVES_IP)}))
+	if (-1 == accept(socketfd, (struct sockaddr*)&serv_sock, &(socklen_t){OFFSETOF(struct sockaddr_in, sin_addr) + sizeof(AALVES_IP)}))
 		perror(DEAMON_NAME " accept()" );
 	ft_putendl_fd("Lol, this is a test", socketfd);
+	ft_putendl_fd("test", 2);
 	return (EXIT_SUCCESS);
 }
